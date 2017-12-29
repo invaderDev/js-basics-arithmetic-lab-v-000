78ef147b5e0b61ee7f202ee9f7686f98694cf683
Kris Thompson <kt.devcat@gmail.com>
2017-12-29 18:01:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,5 @@ const newID = oldID + 1000000000;
 const ageIsValid = Number.isInteger(currentAge);
 
 const randomNumber = Math.random() * 20;
+
+const randomInteger = Math.floor(randomNumber) + 1;
